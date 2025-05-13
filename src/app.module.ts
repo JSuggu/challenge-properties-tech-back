cdf70cba0363b2ffd34a46162ad1cfb9ed3df872
francoazame <franco.azame@techforb.com>
2025-05-13 18:22:33 -0300

    ADD: create role module with entity, dto, service and controller. add properties to the entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
+import { RolesModule } from './modules/roles/roles.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { UsersModule } from './modules/users/users.module';
       synchronize: true,
     }),
     UsersModule,
+    RolesModule,
   ],
   controllers: [],
   providers: [],
