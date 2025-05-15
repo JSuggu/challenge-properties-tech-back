ce08eff2bd906032d97bb0b255e7a47b36fa1650
francoazame <franco.azame@techforb.com>
2025-05-15 19:45:50 -0300

    FIX: fix dto-update in the user and role modules

--- src/modules/roles/dto/update-role.dto.ts
+++ src/modules/roles/dto/update-role.dto.ts
@@ -1,10 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRoleDto } from './create-role.dto';
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
-export class UpdateRoleDto extends PartialType(CreateRoleDto) {
-  @IsString()
-  @IsNotEmpty()
-  @IsOptional()
-  name?: string;
-}
+export class UpdateRoleDto extends PartialType(CreateRoleDto) {}
--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,15 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsEmail, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+import { IsOptional } from 'class-validator';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
-    @IsEmail()
-    @IsNotEmpty()
     @IsOptional()
-    email?: string;
-
-    @IsString()
-    @IsOptional()
-    @MinLength(8, {message: "La contraseña debe tener minimo 8 caracteres"})
-    password?: string;
+    roleId?: never;
 }
